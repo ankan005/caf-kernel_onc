74760a33b0410cfd72cd58da92176896074501e4
Ankan005 <ankan005@gmail.com>
2020-02-09 06:28:35 +0000

    Enable support for sdFAT filesystem

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Feb 8 11:20:23 UTC 2020"
+#define LINUX_COMPILE_BY "ubuntu"
+#define LINUX_COMPILE_HOST "ip-172-31-24-34"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC)"
