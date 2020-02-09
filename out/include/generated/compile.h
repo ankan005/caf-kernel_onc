fe209b106a681e6c7a19ffa15b413770da8ee8ee
Ankan005 <ankan005@gmail.com>
2020-02-09 18:42:28 +0000

    remove out

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#1 SMP PREEMPT Sun Feb 9 18:39:18 UTC 2020"
-#define LINUX_COMPILE_BY "ubuntu"
-#define LINUX_COMPILE_HOST "ip-172-31-24-34"
-#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC)"
